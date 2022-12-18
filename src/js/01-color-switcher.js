280d79516d73f6c16472d00b195649c39baa4c0d
RomanSyhlianyk <romansyglianyk@gmail.com>
2022-12-18 15:06:46 +0200

    Готовий 1 таск

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-console.log(test)
+refs = {
+  buttonStartEl: document.querySelector('button[data-start]'),
+  buttonStopEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let timerId = 0;
+
+refs.buttonStopEl.addEventListener('click', ofChangeColors);
+refs.buttonStartEl.addEventListener('click', onChangeColors);
+
+function onChangeColors() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 2000);
+  refs.buttonStartEl.disabled = true;
+  refs.buttonStopEl.disabled = false;
+}
+
+function ofChangeColors(e) {
+  clearInterval(timerId);
+  refs.buttonStopEl.disabled = true;
+  refs.buttonStartEl.disabled = false;
+}
